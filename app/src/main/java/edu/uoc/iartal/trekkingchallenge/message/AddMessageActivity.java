21c1a3852384a3a317dd05de1c5d9e0c54391414
iartal <iartal@uoc.edu>
2018-01-10 11:52:29 +0100

    FINAL DELIVERY!

--- app/src/main/java/edu/uoc/iartal/trekkingchallenge/message/AddMessageActivity.java
+++ app/src/main/java/edu/uoc/iartal/trekkingchallenge/message/AddMessageActivity.java
@@ -1,3 +1,18 @@
+/* Copyright 2018 Ingrid Artal Hermoso
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 package edu.uoc.iartal.trekkingchallenge.message;
 
 import android.content.Intent;
@@ -39,7 +54,6 @@ public class AddMessageActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.addMessageToolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle(getString(R.string.addMessageActivity));
 
         // Hide keyboard until user select edit text
